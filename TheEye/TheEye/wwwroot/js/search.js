691325a60009ffc8d406ad941e9e004f25a80200
İsmet KİZGİN <ismetkizgin@hotmail.com>
2019-11-29 14:28:28 +0300

    -

---
+++ TheEye/TheEye/wwwroot/js/search.js
@@ -0,0 +1,11 @@
+﻿(function ($) {
+    $("#input-text").on("keyup", function () {
+        $("#input-text").on("keyup", function () {
+            var value = $(this).val().toLowerCase();
+            value=value.trim();
+            $("h5").filter(function () {
+                $(this).parents(".card").toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            });
+        });
+    });
+})(jQuery);
